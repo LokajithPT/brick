6f9f883ce71141f5a77ee4d8718ddd8efb029c2e
skedaddle <lokajith@skedaddle>
2026-04-18 03:03:14 +0530

    fix: simplify next config for render

--- brick/frontend/next.config.js
+++ brick/frontend/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  reactStrictMode: false,
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
